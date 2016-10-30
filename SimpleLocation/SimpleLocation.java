f3943d4c6503b8bc5a59d460a39ff228ef690040
Elaine Chan <elaine.chan@outlook.com>
2016-10-30 15:53:28 -0400

    Add overloading methods

--- SimpleLocation/SimpleLocation.java
+++ SimpleLocation/SimpleLocation.java
@@ -4,7 +4,17 @@ public class SimpleLocation
 	public double latitude;
 	public double longitude;
 
-	// This is a constructor: method to create new object
+	// Default constructor: method to create new object without parameters
+	// Constructor is "overloaded"
+	public SimpleLocation
+	{
+
+		this.latitude = 32.9;
+		this.longitude = -117.2;
+
+	}
+
+	// Parameter constructor: method to create new object with parameters
 	public SimpleLocation(double lat, double lon) 
 	{
 
@@ -19,6 +29,13 @@ public class SimpleLocation
 		// "this" is the calling object
 		return getDist(this.latitude, this.longitude,
 			other.latitude, other.longitude);
+
+	}
+
+	public double distance(double otherLat, double otherLon)
+	{
+
+		// Body not shown
 
 	}
 
