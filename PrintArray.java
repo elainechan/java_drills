00b10789ecb5eed977f78ef3aaec87fefdc43e38
Elaine Chan <elaine.chan@outlook.com>
2016-10-31 15:30:10 -0400

    Add new file

---
+++ PrintArray.java
@@ -0,0 +1,21 @@
+/*
+Given an array, A, of N integers, print A's elements in reverse order 
+as a single line of space-separated numbers.
+*/
+
+public class PrintArray {
+
+    public static void print(String[] arr){
+        for(int i = arr.length - 1; i >= 0; i--){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = Integer.parseInt(in.nextLine());
+        String[] arr = in.nextLine().split(" ");
+        in.close();
+        print(arr);
+    }
+}
