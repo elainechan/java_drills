884e3a9c90db6435a902880a2ff64a7ea807ba83
Elaine Chan <elaine.chan@outlook.com>
2016-11-01 13:40:05 -0400

    Add files

--- FindGene/FindGeneSimpleAndTest.java
+++ FindGene/FindGeneSimpleAndTest.java
@@ -9,7 +9,19 @@ public class FindGeneSimpleAndTest
 		// stop codon is "TAA"
 		String result = "";
 		int startIndex = dna.indexOf("ATG");
+		if (startIndex == -1) // no start codon ATG
+		{
+
+			return "";
+
+		}
 		int stopIndex = dna.indexOf("TAA", startIndex+3);
+		if (stopIndex == -1) // no stop codon TAA
+		{
+
+			return "";
+
+		}
 		result = dna.substring(startIndex, stopIndex+3);
 
 		return result;
