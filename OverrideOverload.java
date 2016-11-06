5c262da9063b3e4417d2073869ab63d26d5c2fb7
Elaine Chan <elaine.chan@outlook.com>
2016-11-06 13:51:08 -0500

    Add file

---
+++ OverrideOverload.java
@@ -0,0 +1,73 @@
+// Example 1
+// Overriding
+class Dog 
+{
+	publc void bark()
+	{
+		System.out.println("woof ");
+	}
+
+}
+
+class Hound extends Dog
+{
+	public void sniff()
+	{
+		System.out.println("sniff ");
+	}
+	public void bark()
+	{
+		System.out.println("bowl ");
+	}
+}
+
+// Overloading
+class Dog
+{
+	public void bark()
+	{
+		System.out.println("woof ");
+	}
+	// Overloading method
+	public void bark(int num)
+	{
+		for(int i=0; i<num; i++)
+			System.out.println("woof ");
+	}
+}
+
+//-------------------------
+
+// Example 2
+public class Person
+{
+	private String name;
+	public String toString()
+	{
+		return this.getName();
+	}
+}
+
+Person p = new Person("Tim");
+System.out.println(p);
+
+// Overriding
+public class Student extends Person
+{
+	private int studentID;
+	public int getSID()
+	{
+		return studentID;
+	}
+	public String toString()
+	{
+		// Uses method from superclass
+		return this.getSID() + ": " + super.toString();
+	}
+}
+
+Student s = new Student("Cara", 1234);
+System.out.println(s);
+
+Person s = new Student("Cara", 1234);
+System.out.println(s);
